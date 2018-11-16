227c338d123c1de1f5362dd3d13002fe2605f87f
EZNLZHI <lindong.zhao@ericsson.com>
2018-11-16 17:17:15 +0800

    shell demo 2018/11/16

--- trust/auto_trust-1.0.sh
+++ trust/auto_trust-1.0.sh
@@ -4,18 +4,19 @@ set timeout 10
 set host [lindex $argv 0]
 set port [lindex $argv 1]
 
-set ip 12035
-spawn scp -P $ip /home/eznlzhi/.ssh/id_rsa.pub root@10.170.7.194:/root/.ssh/id_rsa.pub.1006
+spawn scp -P ${port} /home/eznlzhi/.ssh/id_rsa.pub root@${host}:/root/.ssh/id_rsa.pub.1006
 #expect "yes/no"
 #send "yes\r"
 expect "*Password:"
-send "Root#mps000\r"
+send "Root#mps000"
+send "\r\n"
 expect "*#"
-spawn ssh -p $ip root@10.170.7.194
+spawn ssh -p ${port} root@${host}
 #expect "yes/no"
 #send "yes\r"
 expect "*Password:"
-send "Root#mps000\r"
+send "Root#mps000\r\n"
+send "\r"
 expect "*#"
 send "cat /root/.ssh/id_rsa.pub.1006 >> /root/.ssh/authorized_keys\r"
 expect "*#"
